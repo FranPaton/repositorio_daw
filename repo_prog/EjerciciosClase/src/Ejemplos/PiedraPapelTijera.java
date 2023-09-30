6d07a9919ce349139ed0d6ec1fe055ddd03256ef
FranPaton <franpaton@gmail.com>
2023-09-30 03:06:48 +0200

    Añadida nueva version del juego Piedra, Papel, Tijera

--- repo_prog/EjerciciosClase/src/Ejemplos/PiedraPapelTijera.java
+++ repo_prog/EjerciciosClase/src/Ejemplos/PiedraPapelTijera.java
@@ -6,31 +6,33 @@ public class PiedraPapelTijera {
 	public static void main(String[] args) {
 		
 		int comp = (int)Math.floor((Math.random() * 3));
-		String jugador;
+		String compElige;
+		String jugadorElige;
 		String ganador;
 		Scanner entrada = new Scanner(System.in);
 		
 		System.out.println("Jugador elige: piedra, papel o tijera");
-		jugador = entrada.nextLine();
-		System.out.println(comp == 0 ? "Computadora he elegido piedra" : 
-						   comp == 1 ? "Computadora ha elegido papel" :
-							   			   "Computadora ha elegido tijera"
-						 );
+		jugadorElige = entrada.nextLine();
+		compElige = (comp == 0 ? "piedra" : 
+			   		 comp == 1 ? "papel" :
+			   			 		 "tijera");
+		System.out.println("Computadora ha elegido " + compElige);
 		
 		//System.out.println("Jugador ha elegido " + jugador);					
 		
 		//Piedra = 0; Papel = 1; Tijera = 2
 		
-		ganador = (jugador.equals("piedra") && comp == 1 ? "Ha ganado la computadora" : //Piedra contra papel
-				   jugador.equals("piedra") && comp == 2 ? "Ha ganado el jugador" : //Piedra contra tijera
-				   jugador.equals("papel") && comp == 0 ?  "Ha ganado el jugador" : //Papel contra piedra
-				   jugador.equals("papel") && comp == 2 ?  "Ha ganado la computadorar" : //Papel contra tijera
-				   jugador.equals("tijera") && comp == 0 ? "Ha ganado la computadora" : //Tijera contra piedra
-				   jugador.equals("tijera") && comp == 1 ? "Ha ganado el jugador" : //Tijera contra papel
-					   									   "Empate"					   
+		ganador = (jugadorElige.equals("piedra") && compElige.equals("papel") ?  "Ha ganado la computadora" : //Piedra contra papel gana papel (comp)
+				   jugadorElige.equals("piedra") && compElige.equals("tijera") ? "Ha ganado el jugador" : //Piedra contra tijera gana tijera (jugador)
+				   jugadorElige.equals("papel") && compElige.equals("piedra") ?  "Ha ganado el jugador" : //Papel contra piedra gana papel (jugador)
+				   jugadorElige.equals("papel") && compElige.equals("tijera") ?  "Ha ganado la computadorar" : //Papel contra tijera gana tijera (comp)
+				   jugadorElige.equals("tijera") && compElige.equals("piedra") ? "Ha ganado la computadora" : //Tijera contra piedra gana piedra (comp)
+				   jugadorElige.equals("tijera") && compElige.equals("papel") ?  "Ha ganado el jugador" : //Tijera contra papel gana tijera (jugador)
+					   									   						 "Empate"					   
 			);
 		
 		System.out.println(ganador);
+		
 		
 		entrada.close();
 	}
---
+++ repo_prog/EjerciciosClase/src/Ejemplos/PiedraPapelTijeraV2.java
@@ -0,0 +1,81 @@
+package Ejemplos;
+
+import java.util.*;
+
+public class PiedraPapelTijeraV2 {
+
+	public static void main(String[] args) {
+		
+	    /* La libreria java.util incluye la clase Random para generar números aleatorios, 
+	     * con distintos métodos para generar distintos tipos de datos, al contrario que Math.random(),
+	     * que sólo genera números tipo double.
+	    */
+	
+		/*Random rand = new Random();  
+		  int comp = rand.nextInt(3);
+		*/
+		
+		int comp = (int)Math.floor((Math.random() * 2.999999)); //Se generan doubles de 0 a 3 y se rendondean a la baja para obtener 0, 1 y 2;
+		String compElige;
+		String jugadorElige;
+		String ganador;
+		Scanner entrada = new Scanner(System.in);
+		String piedraAscii = "    _______" 
+					     + "\n---'   ____)" 
+			             + "\n      (_____)" 
+			             + "\n      (_____)" 
+			             + "\n      (____)" 
+			             + "\n---.__(___)"
+			             + "\n  PIEDRA";
+		
+		String papelAscii = "    _______" 
+				         + "\n---'   ____)____" 
+				         + "\n          ______)" 
+				         + "\n          _______)" 
+				         + "\n         _______)" 
+				         + "\n---.__________)  "
+				         + "\n  PAPEL" ;
+		
+		String tijeraAscii = "    _______"
+				         + "\n---'   ____)_____  "
+				         + "\n          _______)"
+				         + "\n       ___________)"
+				         + "\n      (____)"
+				         + "\n---.__(___)"
+				         + "\n  TIJERA";
+	
+		System.out.print("Jugador elige: piedra, papel o tijera: "); //print en lugar de println permite la entrada de datos en la misma línea
+		jugadorElige = entrada.nextLine();
+		
+		compElige = (comp == 0 ? "piedra" : //piedra = 0, papel = 1, tijera = 2 comp
+		             comp == 1 ? "papel" : 
+		            	         "tijera" );
+		 
+		
+		System.out.println("\nJugador elige:");
+		System.out.println(jugadorElige.equals("piedra") ? piedraAscii : 
+						   jugadorElige.equals("papel") ?  papelAscii :
+							   							   tijeraAscii
+						   );
+		
+		System.out.println("\nComputadora elige:");
+		System.out.println(comp == 0 ? piedraAscii : 
+						   comp == 1 ? papelAscii :
+						               tijeraAscii			
+						  );
+		
+		ganador = (jugadorElige.equals("piedra") && compElige.equals("papel") ?  "Papel gana a piedra. Ha ganado la computadora." : //Piedra contra papel gana papel (comp)
+			       jugadorElige.equals("piedra") && compElige.equals("tijera") ? "Piedra gana a tijera. Ha ganado el jugador." : //Piedra contra tijera gana tijera (jugador)
+			       jugadorElige.equals("papel") && compElige.equals("piedra") ?  "Papel gana a piedra. Ha ganado el jugador." : //Papel contra piedra gana papel (jugador)
+			       jugadorElige.equals("papel") && compElige.equals("tijera") ?  "Tijera gana a papel. Ha ganado la computadora." : //Papel contra tijera gana tijera (comp)
+			       jugadorElige.equals("tijera") && compElige.equals("piedra") ? "Piedra gana a tijera. Ha ganado la computadora." : //Tijera contra piedra gana piedra (comp)
+			       jugadorElige.equals("tijera") && compElige.equals("papel") ?  "Tijera gana a papel. Ha ganado el jugador." : //Tijera contra papel gana tijera (jugador)
+				   									   						     "Empate."					   
+		          );
+		
+		System.out.println("\n" + ganador);
+		entrada.close();
+			
+	}
+
+}
