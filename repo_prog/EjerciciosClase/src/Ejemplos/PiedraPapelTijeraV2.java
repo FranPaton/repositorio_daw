f14fba359d3c1a528189f20e6b41aeba148c7317
FranPaton <franpaton@gmail.com>
2023-09-30 06:09:36 +0200

    Subida version v2 de piedra, papel, tijera, con dibujos ASCII

--- repo_prog/EjerciciosClase/src/Ejemplos/PiedraPapelTijeraV2.java
+++ repo_prog/EjerciciosClase/src/Ejemplos/PiedraPapelTijeraV2.java
@@ -46,25 +46,28 @@ public class PiedraPapelTijeraV2 {
 	
 		System.out.print("Jugador elige: piedra, papel o tijera: "); //print en lugar de println permite la entrada de datos en la misma línea
 		jugadorElige = entrada.nextLine();
-		
+			
+		//Convertimos el número generado por el random en su string correspondiente, 0 = piedra, 1 = papel, 2 = tijera, que guardamos en compElige
 		compElige = (comp == 0 ? "piedra" : //piedra = 0, papel = 1, tijera = 2 comp
 		             comp == 1 ? "papel" : 
 		            	         "tijera" );
 		 
 		
-		System.out.println("\nJugador elige:");
+		System.out.println("\nJugador elige:");  //Muestra por pantalla la elección del jugador, representada en código ASCII
 		System.out.println(jugadorElige.equals("piedra") ? piedraAscii : 
 						   jugadorElige.equals("papel") ?  papelAscii :
 							   							   tijeraAscii
 						   );
 		
 		System.out.println("\nComputadora elige:");
-		System.out.println(comp == 0 ? piedraAscii : 
+		
+
+		System.out.println(comp == 0 ? piedraAscii :  //Muestra por pantalla la elección de la computadora, representada en código ASCII
 						   comp == 1 ? papelAscii :
 						               tijeraAscii			
 						  );
 		
-		ganador = (jugadorElige.equals("piedra") && compElige.equals("papel") ?  "Papel gana a piedra. Ha ganado la computadora." : //Piedra contra papel gana papel (comp)
+		/*ganador = (jugadorElige.equals("piedra") && compElige.equals("papel") ?  "Papel gana a piedra. Ha ganado la computadora." : //Piedra contra papel gana papel (comp)
 			       jugadorElige.equals("piedra") && compElige.equals("tijera") ? "Piedra gana a tijera. Ha ganado el jugador." : //Piedra contra tijera gana tijera (jugador)
 			       jugadorElige.equals("papel") && compElige.equals("piedra") ?  "Papel gana a piedra. Ha ganado el jugador." : //Papel contra piedra gana papel (jugador)
 			       jugadorElige.equals("papel") && compElige.equals("tijera") ?  "Tijera gana a papel. Ha ganado la computadora." : //Papel contra tijera gana tijera (comp)
@@ -72,8 +75,16 @@ public class PiedraPapelTijeraV2 {
 			       jugadorElige.equals("tijera") && compElige.equals("papel") ?  "Tijera gana a papel. Ha ganado el jugador." : //Tijera contra papel gana tijera (jugador)
 				   									   						     "Empate."					   
 		          );
+		*/
 		
+		ganador = (jugadorElige.equals(compElige)) ? "Empate " :                    //Mismo resultado, empate
+				  (jugadorElige.equals("piedra") && compElige.equals("tijera")) ||  //Jugador gana, piedra contra tijera 
+				  (jugadorElige.equals("papel") && compElige.equals("piedra") ||    //Jugador gana, papel contra piedra
+				  (jugadorElige.equals("tijera") && compElige.equals("papel")))     //Jugdador gana, tijera contra papel
+	              ? "Gana el jugador." :  "Gana la computadora.";                   // Si no gana el jugador, gana la computadora, el empate ya se resuelve en la primera comparación
+				
 		System.out.println("\n" + ganador);
+		
 		entrada.close();
 			
 	}
