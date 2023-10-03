ee618d625fb1058ef5e1e2f8aa855cd72a4e0b51
FranPaton <franpaton@gmail.com>
2023-10-03 20:11:26 +0200

    Modificado "FormatoHora"

--- repo_prog/EjerciciosClase/src/Ejemplos/FormatoHora.java
+++ repo_prog/EjerciciosClase/src/Ejemplos/FormatoHora.java
@@ -6,14 +6,23 @@ public class FormatoHora {
 
 	public static void main(String[] args) {
 		int hora, minuto, segundo;
+		String formatoHora = " a.m.";
 		Scanner entrada = new Scanner(System.in);
 		
 		System.out.println("Introduce la hora: ");
 		hora = entrada.nextInt();
 		System.out.println("Introduce el mínuto: ");
 		minuto = entrada.nextInt();
-		
 		System.out.println("Introduce el segundo: ");
+		segundo = entrada.nextInt();
+		
+		
+		if (hora > 12) {
+			hora -= 12;
+			formatoHora = " p.m.";		
+		}
+		
+		System.out.println("Son las " + hora + ":" + minuto + ":" + segundo + formatoHora );
 		
 		
 
