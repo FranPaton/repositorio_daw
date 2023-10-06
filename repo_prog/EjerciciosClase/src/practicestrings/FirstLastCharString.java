fa49d8dc9cdd8dd5dc4155fc794ba1f748c1dc53
FranPaton <franpaton@gmail.com>
2023-10-06 07:38:04 +0200

    añadidos ejercicios del boletin de pro y ejercicios de practica de java

---
+++ repo_prog/EjerciciosClase/src/practicestrings/FirstLastCharString.java
@@ -0,0 +1,26 @@
+package practicestrings;
+import java.util.*;
+
+/*
+	Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+	
+	withoutEnd("Hello") → "ell"
+	withoutEnd("java") → "av"
+	withoutEnd("coding") → "odin"
+*/
+
+public class FirstLastCharString {
+
+	public static void main(String[] args) {
+
+		String cadena;
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Introduce cadena: ");
+		cadena = entrada.nextLine();
+		System.out.println(cadena.substring(1, cadena.length() -1));
+				
+
+	}
+
+}
---
+++ repo_prog/EjerciciosClase/src/practicestrings/ShortLongShort.java
@@ -0,0 +1,25 @@
+package practicestrings;
+import java.util.*;
+
+/*
+Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. 
+The strings will not be the same length, but they may be empty (length 0).
+
+comboString("Hello", "hi") → "hiHellohi"
+comboString("hi", "Hello") → "hiHellohi"
+comboString("aaa", "b") → "baaab" 
+*/
+
+public class ShortLongShort {
+
+	public static void main(String[] args) {
+		
+		String a = "aaa";
+		String b = "b";
+		String res;
+		
+		res = a.length() < b.length() ? a + b + a : b + a + b;
+		System.out.println(res);
+	}
+
+}
