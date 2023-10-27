5fbfd7ba2145ba971021c9c5c5aab8c8f63814f6
FranPaton <franpaton@gmail.com>
2023-10-27 20:53:43 +0200

    añadidos ejercicios del boletin 2 y 3 de lenguaje de marcas

---
+++ repo_prog/EjerciciosClase/src/unidad3/NumeroPrimo.java
@@ -0,0 +1,34 @@
+package unidad3;
+
+import java.util.Scanner;
+
+public class NumeroPrimo {
+
+	
+	
+	public static void main(String[] args) {
+		int i = 2, num;
+		Scanner entrada = new Scanner (System.in);
+		boolean esPrimo = true;
+		
+		System.out.println("Introduce un número: ");
+		num = entrada.nextInt();
+		
+		esPrimo = esPrimo(num);
+		System.out.printf((esPrimo ? "%d es primo" : "%d no es primo"), num);
+		entrada.close();
+	}
+
+	private static boolean esPrimo(int num) {
+		int i = 2;
+		boolean esPrimo = true;
+		while(i < num && esPrimo) {
+			if (num % i == 0) {
+				esPrimo = false;
+			}	
+			i++;
+		}
+		return esPrimo;
+	}
+
+}
